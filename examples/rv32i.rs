db95c5005a72d30ef14e8ee7fc0f793a9069685c
Felix Roithmayr <felix.roithmayr@aon.at>
2024-10-25 09:48:13 +0200

    add sample error handling to examples

--- examples/rv32i.rs
+++ examples/rv32i.rs
@@ -1,11 +1,19 @@
 use instruction_decoder::Decoder;
 
 pub fn main() {
-    if let Ok(test_decoder) = Decoder::new(&vec![include_str!("../toml/RV32I.toml").to_string()]) {
-        let inst = 0xff460593;
+    match Decoder::new(&vec![include_str!("../toml/RV32I.toml").to_string()]) {
+        Ok(test_decoder) => {
+            let inst = 0xff460593;
 
-        if let Ok(iform) = test_decoder.decode_from_i64(inst, 32) {
-            println!("{:?}", iform);
+            if let Ok(iform) = test_decoder.decode_from_i64(inst, 32) {
+                println!("{:?}", iform);
+            }
+        }
+        Err(error_stacks) => {
+            println!("Errors in ../toml/RV32I.toml:");
+            for error in &error_stacks[0] {
+                println!("\t{}", error);
+            }
         }
     }
 }
