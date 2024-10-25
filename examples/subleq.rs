591330c3b7e90d6c3b587e64382ef62196aa1d70
Felix Roithmayr <felix.roithmayr@aon.at>
2024-10-25 09:38:36 +0200

    Initial implementation of toml parsing error handling

--- examples/subleq.rs
+++ examples/subleq.rs
@@ -1,10 +1,11 @@
 use instruction_decoder::Decoder;
 
 pub fn main() {
-    let test_decoder = Decoder::new(&vec![include_str!("../toml/subleq.toml").to_string()]);
-    let inst = 0x1204;
+    if let Ok(test_decoder) = Decoder::new(&vec![include_str!("../toml/subleq.toml").to_string()]) {
+        let inst = 0x1204;
 
-    if let Ok(iform) = test_decoder.decode_from_i64(inst, 16) {
-        println!("{:?}", iform);
+        if let Ok(iform) = test_decoder.decode_from_i64(inst, 16) {
+            println!("{:?}", iform);
+        }
     }
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,3 +1,4 @@
+mod error;
 mod mips;
 mod rv32;
 mod rv64;
